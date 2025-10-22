44d8c8f5534e2c353f6c903d3cede8844d9618db
Mark Spreeuwenberg <m.spreeuwenberg@tools4ever.com>
2025-10-22 08:49:22 +0200

    fix: filter so it doesn't match on exactly 14 days

--- All-in-one setup/createform.ps1
+++ All-in-one setup/createform.ps1
@@ -330,7 +330,7 @@ try{
             [datetime]$ConvertDate = $adUser.ExpiryDate
             $ExpireDate = $ConvertDate.ToShortDateString()
         
-            If ($ExpireDate -eq $dateBeforeExpire) {
+            If ($ExpireDate -ne "1/1/1601" -and [datetime]$ExpireDate -lt [datetime]$dateBeforeExpire) {
                 $formattedDate = $ConvertDate.ToString("dd-MM-yyyy")
                 $adUser | Add-Member -MemberType NoteProperty -Name FormattedDate -Value $formattedDate -Force
 
@@ -415,7 +415,7 @@ foreach($category in $delegatedFormCategories) {
         $uri = ($script:PortalBaseUrl +"api/v1/delegatedformcategories/$category")
         $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $script:headers -ContentType "application/json" -Verbose:$false
         $response = $response | Where-Object {$_.name.en -eq $category}
-	
+        
         $tmpGuid = $response.delegatedFormCategoryGuid
         $delegatedFormCategoryGuids += $tmpGuid
         
